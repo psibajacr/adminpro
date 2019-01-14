408a7f8e34c292497d7760d6fb4c2e9cb09b2d46
Pablo Sibaja Villegas <psibaja@coopenae.fi.cr>
2019-01-14 07:26:00 -0600

    Fin de la sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import { FormsModule } from '@angular/forms';
-
-
+import { ServiceModule } from './services/service.module';
 
 
 @NgModule({
@@ -25,7 +24,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
